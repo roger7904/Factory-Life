334a36e24fb0f6b0ed4e4b3767769d9a622e78c1
tzuyen <angela19990807@gmail.com>
2020-06-14 11:01:14 +0800

    teachingscene串接完畢

---
+++ KeepDoIt/Assets/Resources/btnlistener/exit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class exit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick(){
+        System.Environment.Exit(System.Environment.ExitCode);
+    }
+}
